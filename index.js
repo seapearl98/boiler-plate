efa5929d9d64079d7413383f76c19adb88be865c
☀️ <sun@ihaejuui-MacBookAir.local>
2025-06-10 00:27:36 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+const port = 3000;
+const { User } = require("./models/User");
 
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+//application/json
+app.use(express.json());
+require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://gown1977:fjqm74159@cluster0.uy2unfc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    // {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    //   useCreateIndex: true,
-    //   useFindAndModify: false,
-    // }
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", async (req, res) => {
+  //회원가입할때 필요한 정보들을 client에서 가져오면
+  //db에 넣어주기
+  // const user = new User(req.body);
+  // user.save((err, userInfo) => {
+  //   if (err) return res.json({ success: false, err });
+  //   return res.status(200).json({
+  //     success: true,
+  //   });
+  // }); 예전 방법임
+  try {
+    const user = new User(req.body);
+    await user.save();
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.status(400).json({ success: false, err });
+  }
 });
 
 app.listen(port, () => {
